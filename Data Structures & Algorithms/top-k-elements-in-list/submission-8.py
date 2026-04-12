dab5eec96df7a7c1b2c2dc0a19a6164510dde2fa
stevenliem7 <100023156+stevenliem7@users.noreply.github.com>
2026-04-12 22:48:14 +1000

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,31 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = {i:[] for i in range(len(nums) + 1)}
+        mapp = {}
+        print(mapp)
+        sol = []
+        
+        for num in nums:
+            if num in mapp: mapp[num] += 1
+            else: mapp[num] = 1
+        
+        for key,v in mapp.items():
+            heap[v].append(key)
+        print(heap)
+
+        for i in range(len(heap) - 1, 0, -1):
+            if len(sol) == k:
+                return sol
+            temp_val = heap.get(i)
+            if temp_val is not None:
+                for elem in temp_val:
+                    sol.append(elem)
+        
+        return sol
+            
+        
+
+
+        
