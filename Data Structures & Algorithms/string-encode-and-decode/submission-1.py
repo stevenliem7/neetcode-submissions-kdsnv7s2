eae26dfc4f4d62475a34c1a94605b8e2fa1442d5
stevenliem7 <100023156+stevenliem7@users.noreply.github.com>
2026-04-13 00:12:37 +1000

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        payload = ""
+        for word in strs:
+            payload += f"{len(word)}#{word}"
+        print(payload)
+        return payload
+
+    def decode(self, s: str) -> List[str]:
+        payload = []
+        # 10#HelloWorld5#World
+        i=0
+        while i < len(s):
+            temp_count = i
+            while s[temp_count] != "#" :
+                temp_count += 1
+            print(s[i], s[i+1])
+            substring_counter = int(s[i:temp_count])
+
+            print("This " + s[i+len(str(substring_counter)) + 1: i+substring_counter+len(str(substring_counter))+1])
+            payload.append(s[i+len(str(substring_counter)) + 1: i+substring_counter+len(str(substring_counter))+1])
+            i += substring_counter + len(str(substring_counter)) + 1
+
+
+        return payload
+
