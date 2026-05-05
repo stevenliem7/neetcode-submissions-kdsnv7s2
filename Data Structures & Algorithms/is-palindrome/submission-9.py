ef2932cc49de0e91edbd7fb693db918e6264c6d1
stevenliem7 <100023156+stevenliem7@users.noreply.github.com>
2026-05-05 12:41:11 +1000

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_new = ""
+        for letter in s:
+            if letter.isalpha() or letter.isnumeric():
+                s_new += letter.lower()
+        
+        start = 0
+        s_len = len(s_new)
+        end = s_len - 1
+
+        while start < end:
+            if s_new[start] != s_new[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+
+        
